61ba49222718f49e32ef44a17e36d826a2882a1a
rizwaradnan03 <rizwar.adnanbyantara@gmail.com>
2025-05-04 22:35:33 +0700

    .

--- updateStructure.js
+++ updateStructure.js
@@ -14,25 +14,24 @@ await client.connect();
 
 const workBook = {
   userPayment: xlsx.readFile("user_payments.xls"),
-  user : xlsx.readFile("users.xls"),
-  productCategories : xlsx.readFile("product_categories.xls"),
-  products : xlsx.readFile("products.xls"),
-  personalInformation : xlsx.readFile("personal_infomations.xls"),
-  parqQuestion : xlsx.readFile("parq_questions.xls"),
-  midtrans : xlsx.readFile("transaction-report.xls"),
-}
+  user: xlsx.readFile("users.xls"),
+  productCategories: xlsx.readFile("product_categories.xls"),
+  products: xlsx.readFile("products.xls"),
+  personalInformation: xlsx.readFile("personal_infomations.xls"),
+  parqQuestion: xlsx.readFile("parq_questions.xls"),
+  midtrans: xlsx.readFile("transaction-report.xls"),
+};
 
 const workSheet = {
   userPayment: workBook.userPayment.Sheets["user_payments"],
   user: workBook.user.Sheets["users"],
-  productCategories :
-  workBook.productCategories.Sheets["product_categories"],
+  productCategories: workBook.productCategories.Sheets["product_categories"],
   products: workBook.products.Sheets["products"],
-  personalInformation :
-  workBook.personalInformation.Sheets["personal_infomations"],
+  personalInformation:
+    workBook.personalInformation.Sheets["personal_infomations"],
   parqQuestion: workBook.parqQuestion.Sheets["parq_questions"],
   midtrans: workBook.midtrans.Sheets["transaction-report"],
-}
+};
 
 const data = {
   userPayment:
@@ -40,15 +39,14 @@ const data = {
   user: xlsx.utils.sheet_to_json(workSheet.user, { header: 1 }) || [],
   productCategories:
     xlsx.utils.sheet_to_json(workSheet.productCategories, { header: 1 }) || [],
-  products:
-    xlsx.utils.sheet_to_json(workSheet.products, { header: 1 }) || [],
+  products: xlsx.utils.sheet_to_json(workSheet.products, { header: 1 }) || [],
   personalInformation:
-    xlsx.utils.sheet_to_json(workSheet.personalInformation, { header: 1 }) || [],
+    xlsx.utils.sheet_to_json(workSheet.personalInformation, { header: 1 }) ||
+    [],
   parqQuestion:
     xlsx.utils.sheet_to_json(workSheet.parqQuestion, { header: 1 }) || [],
-  midtrans:
-    xlsx.utils.sheet_to_json(workSheet.midtrans, { header: 1 }) || [],
-}
+  midtrans: xlsx.utils.sheet_to_json(workSheet.midtrans, { header: 1 }) || [],
+};
 
 let eventQuery = [];
 let productCategoriesQuery = [];
@@ -114,29 +112,29 @@ for (let i = 1; i < data.personalInformation.length; i++) {
   if (!isDuplicated) {
     personalInformationsQuery.push([
       i,
-      `INSERT INTO personal_infomations (id, user_id, first_name, last_name, contact, gender, nationality, ktp, health_problem, community, name_of_emergency_contact, relation_of_emergency_contact, "emergencyContact", shirt_type, shirt_size, blood_type, created_at, updated_at) VALUES ('${data.personalInformation[i][0]}', '${data.personalInformation[i][1]}', '${data.personalInformation[i][2]}', '${data.personalInformation[i][3]}', '${data.personalInformation[i][4]}', '${data.personalInformation[i][5]}', '${data.personalInformation[i][6]}', '${data.personalInformation[i][7]}', '${data.personalInformation[i][8]}', '${data.personalInformation[i][11]}', '${data.personalInformation[i][12]}', '${data.personalInformation[i][13]}', '${data.personalInformation[i][14]}', '${data.personalInformation[i][15]}', '${data.personalInformation[i][16]}', '${data.personalInformation[i][9]}', '2025-03-20', '2025-03-20');`,
+      `INSERT INTO personal_infomations (id, user_id, first_name, last_name, contact, gender, nationality, ktp, health_problem, community, name_of_emergency_contact, relation_of_emergency_contact, emergency_contact, shirt_type, shirt_size, blood_type, created_at, updated_at) VALUES ('${data.personalInformation[i][0]}', '${data.personalInformation[i][1]}', '${data.personalInformation[i][2]}', '${data.personalInformation[i][3]}', '${data.personalInformation[i][4]}', '${data.personalInformation[i][5]}', '${data.personalInformation[i][6]}', '${data.personalInformation[i][7]}', '${data.personalInformation[i][8]}', '${data.personalInformation[i][11]}', '${data.personalInformation[i][12]}', '${data.personalInformation[i][13]}', '${data.personalInformation[i][14]}', '${data.personalInformation[i][15]}', '${data.personalInformation[i][16]}', '${data.personalInformation[i][9]}', '2025-03-20', '2025-03-20');`,
     ]);
     personalInformationIds.push(data.personalInformation[i][0]);
   }
 }
 
-let unValid = []
+let unValid = [];
 
-let usedOrderId = []
+let usedOrderId = [];
 for (let i = 1; i < data.userPayment.length; i++) {
-  let isValid = false
+  let isValid = false;
 
-  if(data.userPayment[i][1] && data.userPayment[i][4] == true){
-    isValid = true
+  if (data.userPayment[i][1] && data.userPayment[i][4] == true) {
+    isValid = true;
     userPaymentQuery.push([
       i,
       `INSERT INTO user_payments (id, order_id, variant_id, user_id, transaction_status, is_payed, is_qr_booked, gross_amount, payment_type, created_at, updated_at, bib_number) VALUES('${
         data.userPayment[i][0]
       }', ${data.userPayment[i][1] ? `'${data.userPayment[i][1]}'` : null}, '${
         data.userPayment[i][2]
-      }', '${data.userPayment[i][3]}', '${
-        "settlement"
-      }',${data.userPayment[i][4]}, ${data.userPayment[i][5]}, ${
+      }', '${data.userPayment[i][3]}', '${"settlement"}',${
+        data.userPayment[i][4]
+      }, ${data.userPayment[i][5]}, ${
         data.userPayment[i][6] ? data.userPayment[i][6] : null
       }, ${
         data.userPayment[i][7] ? `'${data.userPayment[i][7]}'` : null
@@ -146,52 +144,50 @@ for (let i = 1; i < data.userPayment.length; i++) {
           : null
       });`,
     ]);
-  }else{
-    const userId = data.userPayment[i][3]
+  } else {
+    const userId = data.userPayment[i][3];
 
-    for(let j = 1;j < data.user.length;j++){
-      let isStop = false
+    for (let j = 1; j < data.user.length; j++) {
+      let isStop = false;
 
-      if(userId == data.user[j][0]){
-        for(let p = 1;p < data.midtrans.length;p++){
-          if(data.midtrans[p][8] == data.user[j][2] && data.midtrans[p][5] != "settlement" && data.midtrans[p][3] == "Payment"){
-            isValid = true
-            let isSame = false
+      if (userId == data.user[j][0]) {
+        for (let p = 1; p < data.midtrans.length; p++) {
+          if (
+            data.midtrans[p][8] == data.user[j][2] &&
+            data.midtrans[p][5] != "settlement" &&
+            data.midtrans[p][3] == "Payment"
+          ) {
+            isValid = true;
+            let isSame = false;
 
-            for(let k = 0;k < usedOrderId.length;k++){
-              if(data.midtrans[p][1] == usedOrderId[k]){
-                isSame = true
+            for (let k = 0; k < usedOrderId.length; k++) {
+              if (data.midtrans[p][1] == usedOrderId[k]) {
+                isSame = true;
               }
             }
 
-            if(!isSame){
+            if (!isSame) {
               userPaymentQuery.push([
                 i,
-                `INSERT INTO user_payments (id, order_id, variant_id, user_id, transaction_status, is_payed, is_qr_booked, gross_amount, payment_type, created_at, updated_at, bib_number) VALUES('${
-                  data.userPayment[i][0]
-                }', '${data.midtrans[p][1]}', '${
-                  data.userPayment[i][2]
-                }', '${data.userPayment[i][3]}', '${data.midtrans[p][5]}',${data.userPayment[i][4]}, ${data.userPayment[i][5]}, ${
-                  data.midtrans[p][4]
-                },'${data.midtrans[p][3]}', '2025-03-21', '2025-03-21', null);`,
+                `INSERT INTO user_payments (id, order_id, variant_id, user_id, transaction_status, is_payed, is_qr_booked, gross_amount, payment_type, created_at, updated_at, bib_number) VALUES('${data.userPayment[i][0]}', '${data.midtrans[p][1]}', '${data.userPayment[i][2]}', '${data.userPayment[i][3]}', '${data.midtrans[p][5]}',${data.userPayment[i][4]}, ${data.userPayment[i][5]}, ${data.midtrans[p][4]},'${data.midtrans[p][3]}', '2025-03-21', '2025-03-21', null);`,
               ]);
-  
-              usedOrderId.push(data.midtrans[p][1])
-              isStop = true
-              break
+
+              usedOrderId.push(data.midtrans[p][1]);
+              isStop = true;
+              break;
             }
           }
         }
       }
 
-      if(isStop){
-        break
+      if (isStop) {
+        break;
       }
     }
   }
 
-  if(!isValid){
-    unValid.push(data.userPayment[i][3])
+  if (!isValid) {
+    unValid.push(data.userPayment[i][3]);
   }
 }
 
@@ -200,15 +196,13 @@ for (let i = 1; i < data.userPayment.length; i++) {
 // for(let i = 0;i < unValid.length;i++){
 //   for(let j = 1;j < data.user.length;j++){
 //     if(unValid[i] == data.user[j][0]){
-      
+
 //       for(let p = 1;p < data.midtrans.length;p++){
 //         if(data.midtrans[p][8] == data.user[j][2]){
-//           query.push([i+1, 
+//           query.push([i+1,
 // `INSERT INTO transactions (id, event_id, created_at, updated_at) VALUES ('${data.midtrans[p][1]}', '3d3b8134-9d02-4180-ab51-d6cd236df1e2','2025-03-21', '2025-03-21');`])
-          
-      
 
-//       query.push([i+1, 
+//       query.push([i+1,
 // `UPDATE user_payments SET transaction_id = '${data.midtrans[p][1]}', is_payed = true, is_qr_booked = false, gross_amount = ${data.midtrans[p][4]}, payment_type = '${data.midtrans[p][2]}', bib_number = '0218' where id = 'cebfd198-5de8-4bf3-913b-4ce09853c53c';`])
 //         }
 //       }
@@ -218,14 +212,14 @@ for (let i = 1; i < data.userPayment.length; i++) {
 //   }
 // }
 
-// console.log("Query : ", query)
+// console.log("Query : ", personalInformationsQuery[0]);
 
 const queries = async () => {
   let eventLogs = [];
   for (let i = 0; i < eventQuery.length; i++) {
     try {
       await client.query(eventQuery[i][1]);
-      console.log("Event Log : ", i);
+      // console.log("Event Log : ", i);
       eventLogs.push([i, "success"]);
     } catch (error) {
       eventLogs.push([i, "failed"]);
@@ -236,7 +230,7 @@ const queries = async () => {
   for (let i = 0; i < productCategoriesQuery.length; i++) {
     try {
       await client.query(productCategoriesQuery[i][1]);
-      console.log("Product Category Log : ", i);
+      // console.log("Product Category Log : ", i);
       productCategoryLogs.push([i, "success"]);
     } catch (error) {
       productCategoryLogs.push([i, "failed"]);
@@ -247,7 +241,7 @@ const queries = async () => {
   for (let i = 0; i < productsQuery.length; i++) {
     try {
       await client.query(productsQuery[i][1]);
-      console.log("Product Log : ", i);
+      // console.log("Product Log : ", i);
       productLogs.push([i, "success"]);
     } catch (error) {
       productLogs.push([i, "failed"]);
@@ -258,7 +252,7 @@ const queries = async () => {
   for (let i = 0; i < variantsQuery.length; i++) {
     try {
       await client.query(variantsQuery[i][1]);
-      console.log("Variant Log : ", i);
+      // console.log("Variant Log : ", i);
       variantLogs.push([i, "success"]);
     } catch (error) {
       variantLogs.push([i, "failed"]);
@@ -269,7 +263,7 @@ const queries = async () => {
   for (let i = 0; i < usersQuery.length; i++) {
     try {
       await client.query(usersQuery[i][1]);
-      console.log("User Log : ", i);
+      // console.log("User Log : ", i);
       userLogs.push([i, "success"]);
     } catch (error) {
       userLogs.push([i, "failed"]);
@@ -280,7 +274,7 @@ const queries = async () => {
   for (let i = 0; i < parqQuestionsQuery.length; i++) {
     try {
       await client.query(parqQuestionsQuery[i][1]);
-      console.log("Parq Question Log : ", i);
+      // console.log("Parq Question Log : ", i);
       parqQuestionLogs.push([i, "success"]);
     } catch (error) {
       parqQuestionLogs.push([i, "failed"]);
@@ -302,13 +296,15 @@ const queries = async () => {
   for (let i = 0; i < userPaymentQuery.length; i++) {
     try {
       await client.query(userPaymentQuery[i][1]);
-      console.log("User Payment Log : ", i);
+      // console.log("User Payment Log : ", i);
       userPaymentLogs.push([i, "success"]);
     } catch (error) {
       userPaymentLogs.push([i, "failed"]);
     }
   }
 };
+
+console.log("Personal Info Length : ", personalInformationsQuery.length)
 
 // queries()
 
